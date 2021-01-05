3274973e59261a5d883dce88da5d8061193a7264
Roy O'Young <roy2220@outlook.com>
2021-01-05 11:45:46 +0800

    Comment example

--- example_test.go
+++ example_test.go
@@ -13,13 +13,13 @@ import (
 func ExampleBackoff() {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	_ = cancel
-	backoff := backoff.New(backoff.Options{
-		MinDelay:            100 * time.Millisecond,
-		MaxDelay:            100 * time.Second,
-		DelayFactor:         2,
-		MaxDelayJitter:      1,
-		DelayFunc:           backoff.DelayWithContext(ctx),
-		MaxNumberOfAttempts: 100,
+	b := backoff.New(backoff.Options{
+		MinDelay:            100 * time.Millisecond,        // default
+		MaxDelay:            100 * time.Second,             // default
+		DelayFactor:         2,                             // default
+		MaxDelayJitter:      1,                             // default
+		DelayFunc:           backoff.DelayWithContext(ctx), // with respect to ctx
+		MaxNumberOfAttempts: 100,                           // default
 	})
 	req, err := http.NewRequestWithContext(ctx, "GET", "http://example.com/", nil)
 	if err != nil {
@@ -28,20 +28,20 @@ func ExampleBackoff() {
 	for {
 		resp, err := http.DefaultClient.Do(req)
 		if err != nil {
-			if err2 := backoff.Do(); err2 != nil {
+			if err2 := b.Do(); err2 != nil { // delay
 				log.Printf("failed to back off; err=%q", err2)
 				log.Fatal(err)
 			}
-			continue
+			continue // retry
 		}
 		resp.Body.Close()
 		if resp.StatusCode/100 == 5 {
 			err := fmt.Errorf("http server error; httpStatusCode=%v", resp.StatusCode)
-			if err2 := backoff.Do(); err2 != nil {
+			if err2 := b.Do(); err2 != nil { // delay
 				log.Printf("failed to back off; err=%q", err2)
 				log.Fatal(err)
 			}
-			continue
+			continue // retry
 		}
 		fmt.Println(resp.StatusCode)
 		return
